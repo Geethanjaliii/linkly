312fa94adc45169efc686752597e04ff69548068
Geethanjali V N <geethanjaligeethanjali23@gmail.com>
2026-08-01 23:41:58 +0530

    Update url_service.py

--- backend/src/services/url_service.py
+++ backend/src/services/url_service.py
@@ -6,7 +6,7 @@ from src.models import models
 def generate_unique_short_code(db: Session) -> str:
     alphabet = string.ascii_letters + string.digits
     while True:
-        #Generate a code of random length between 6 8
+        #Generate a code of random length between 6,8
         length = secrets.choice([6, 7, 8])
         code = "".join(secrets.choice(alphabet) for _ in range(length))
         
